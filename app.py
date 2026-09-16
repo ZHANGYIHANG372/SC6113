2c9256063b2f0651aa1cdffafe22281485bd9fc2
ZHANGYIHANG372 <13582071858@163.com>
2026-09-16 12:48:26 +0000

    1

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
+@app.route("/main",methods=['GET','POST'])
+def main():
+  return(render_template("main.html"))
+
+@app.route("/transfermoney",methods=['GET','POST'])
+def transfermoney():
+  return(render_template("transfermoney.html"))
+
 if __name__ == "__main__":
   app.run() 
